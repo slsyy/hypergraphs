0c4bab537593f12dd1b911a12251aede443ed8dc
Rockfan121 <k.mura121@gmail.com>
2019-01-10 03:55:21 +0100

    Implement missing tests for p5

--- hypergraphs/utils.py
+++ hypergraphs/utils.py
@@ -9,6 +9,13 @@ class HyperEdge(Enum):
     B = 'B'
 
 
+class Direction(Enum):
+    N = 1
+    S = 2
+    W = 3
+    E = 4
+
+
 def get_node_id(node_position: Tuple[int, int]) -> int:
     """
     :param node_position: a tuple containing x and y coordinates of a node
@@ -28,21 +35,30 @@ def get_common_nodes(graph, hyperedge_id):
 
 
 def get_f1_nodes(graph, common_node_id):
+    allnodes =  [ data for idd, data in graph.nodes(data=True)]
+    tmpnodes = __get_x_nodes(graph, common_node_id, Direction.S)
+    tmp2nodes = __get_x_nodes(graph, common_node_id, Direction.N)
+    nodes = __get_x_nodes(graph, common_node_id, HyperEdge.F1)
     return __get_x_nodes(graph, common_node_id, HyperEdge.F1)
 
 
 def get_f2_nodes(graph, common_node_id):
+    allnodes = [ data for idd, data in graph.nodes(data=True)]
+    tmpnodes = __get_x_nodes(graph, common_node_id, Direction.W)
+    tmp2nodes = __get_x_nodes(graph, common_node_id, Direction.E)
+    nodes = __get_x_nodes(graph, common_node_id, HyperEdge.F2)
     return __get_x_nodes(graph, common_node_id, HyperEdge.F2)
 
 
 def get_i_nodes(graph, common_node_id):
-    return __get_x_nodes(graph, common_node_id, HyperEdge.I)
+    nodes = __get_x_nodes(graph, common_node_id, HyperEdge.I.name)
+    return __get_x_nodes(graph, common_node_id, HyperEdge.I.name)
 
 
 def __get_x_nodes(graph, common_node_id, label):
     if graph.node[common_node_id]['is_hyperedge']:
         raise ValueError('Given node_id is not id of common node')
-    return [x_node for x_node in graph[common_node_id] if graph.nodes[x_node]['label'] == label.name]
+    return [x_node for x_node in graph[common_node_id] if graph.nodes[x_node]['label'] == label]
 
 
 def common_elements(list1, list2):
